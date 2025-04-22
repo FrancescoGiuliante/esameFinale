99c8b0232793d4cee042de657512972a523b5f44
Francesco Giuliante <francesco.giuliante@hotmail.it>
2025-04-22 12:07:46 +0200

    first commit

---
+++ src/test/java/com/francescogiuliante/esameFinale/EsameFinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francescogiuliante.esameFinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameFinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
